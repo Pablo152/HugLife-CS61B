5539086c6364956afaad9db12f6b776185c86016
Pablo <pablo45154@gmail.com>
2020-09-12 16:54:46 -0400

    Update Clorus.java

--- creatures/Clorus.java
+++ creatures/Clorus.java
@@ -20,7 +20,7 @@ public class Clorus extends Creature {
     private int b;
 
     /**
-     * creates plip with energy equal to E.
+     * creates Clorus with energy equal to E.
      */
     public Clorus(double e) {
         super("clorus");
@@ -29,34 +29,60 @@ public class Clorus extends Creature {
         b = 0;
         energy = e;
     }
-
+    
+    /**
+    * creates Clorus with energy 1
+    */
+    
     public Clorus() {
         this(1);
     }
 
-
+    
+    /**
+    * Sets clorus color
+    */
+    
     public Color color() {
         r = 34;
         b = 231;
         g = 0;
         return color(r, g, b);
     }
-
+    
+    /**
+    * Consumes the energy of creature it attacks.
+    */
 
     public void attack(Creature c) {
         this.energy += c.energy();
     }
 
+    /**
+    * looses 0.03 energy for every move it makes.
+    */
+    
     public void move() {
         this.energy -= 0.03;
         if (this.energy < 0)
             this.energy = 0;
     }
 
-
+    /**
+    * looses 0.01 energy if it stays.
+    */
+    
     public void stay() {
         this.energy -= 0.01;
     }
+    
+    /**
+    * Chooses an action based on different rules
+    * Rule 1: if neighbour cells are empty, the Clorus stays.
+    * Rule 2: if neighbour cells have plips, attack a random cell which has a plip.
+    * Rule 3: if energy is greater than or equal to 1, the Clorus replicates itself.
+    * Rule 4: Otherwise, move to a random empty cell.
+    */
 
     @Override
     public Action chooseAction(Map<Direction, Occupant> neighbors) {
@@ -87,7 +113,10 @@ public class Clorus extends Creature {
         return new Action(Action.ActionType.MOVE, HugLifeUtils.randomEntry(emptyNeighbors));
 
     }
-
+    
+    /**
+    * Creates a new clorus and gives his offspring half his energy.
+    */
 
     public Clorus replicate() {
         Clorus clorusClone = new Clorus(this.energy / 2);
